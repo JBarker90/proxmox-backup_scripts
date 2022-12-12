6dc85dccbbbf8a8da3027c75c1425816dad4ef91
JBarker09 <jonathan.barker09@gmail.com>
2022-12-12 17:29:21 -0500

    created current backup date variable

--- backup-archive_test.sh
+++ backup-archive_test.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 # This backup script is used to archive ProxMox PVE backups into their own directories
+
+BACKUP_DATE="$(ls -lash /home/jonathan/backup_test/dump | grep 'vzdump' | awk -F '-' '{print $8, $9, $10}' | sed 's/_/-/g' | awk '{print $3}' | sort | tail -n1)"
+
+echo "$BACKUP_DATE"
