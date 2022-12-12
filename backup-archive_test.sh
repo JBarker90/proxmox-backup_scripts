0ae57c207546ecfc6f3548ae8f1c55a564a35510
JBarker09 <jonathan.barker09@gmail.com>
2022-12-12 18:07:07 -0500

    modified backup date

--- backup-archive_test.sh
+++ backup-archive_test.sh
@@ -2,6 +2,7 @@
 
 # This backup script is used to archive ProxMox PVE backups into their own directories
 
-BACKUP_DATE="$(ls -lash /home/jonathan/backup_test/dump | grep 'vzdump' | awk -F '-' '{print $8, $9, $10}' | sed 's/_/-/g' | awk '{print $3}' | sort | tail -n1)"
+BACKUP_PATH="/home/jonathan/backup_test/dump"
+BACKUP_DATE="$(find "$BACKUP_PATH" -maxdepth 1 -type f -iname 'vzdump*' | awk -F '/' '{print $6}' | cut -d '-' -f 4 | sed 's/_/-/g' | sort | tail -n1)"
 
 echo "$BACKUP_DATE"
