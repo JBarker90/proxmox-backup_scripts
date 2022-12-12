957762cde32151d2f6ea12271a608724bd9272f5
JBarker09 <jonathan.barker09@gmail.com>
2022-12-12 18:35:43 -0500

    added lines to createdirectories for vm/lxc

--- backup-archive_test.sh
+++ backup-archive_test.sh
@@ -5,11 +5,13 @@
 BACKUP_PATH="/home/jonathan/backup_test/dump"
 BACKUP_DATE="$(find "$BACKUP_PATH" -maxdepth 1 -type f -iname 'vzdump*' | awk -F '/' '{print $6}' | cut -d '-' -f 4 | sed 's/_/-/g' | sort | tail -n1)"
 
-#echo "$BACKUP_DATE"
+# This will check if there is a parent directory for the current backups and then create a directory
 if [[ -e "$BACKUP_PATH/monthly_backups.$BACKUP_DATE" ]]; then
     echo "This directory already exists..."
 
 else
 
     mkdir $BACKUP_PATH/monthly_backups."$BACKUP_DATE"
+    mkdir $BACKUP_PATH/monthly_backups."$BACKUP_DATE"/vm_backups."$BACKUP_DATE"
+    mkdir $BACKUP_PATH/monthly_backups."$BACKUP_DATE"/lxc_backups."$BACKUP_DATE"
 fi
