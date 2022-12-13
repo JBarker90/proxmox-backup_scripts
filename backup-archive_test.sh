e6e3e23a2b4a7a9ee2873de4695ecd4681faaaf7
JBarker09 <jonathan.barker09@gmail.com>
2022-12-13 12:29:36 -0500

    created new variable to find lxc files and tested it

--- backup-archive_test.sh
+++ backup-archive_test.sh
@@ -5,6 +5,7 @@
 BACKUP_PATH="/home/jonathan/backup_test/dump"
 BACKUP_DATE="$(find "$BACKUP_PATH" -maxdepth 1 -type f -iname 'vzdump*' | awk -F '/' '{print $6}' | cut -d '-' -f 4 | sed 's/_/-/g' | sort | tail -n1)"
 VM_FILES="$(find "$BACKUP_PATH" -maxdepth 1 -type f -iname '*qemu*')"
+LXC_FILES="$(find "$BACKUP_PATH" -maxdepth 1 -type f -iname '*lxc*')"
 
 
 # This will check if there is a parent directory for the current backups and then create a directory
@@ -16,5 +17,5 @@ else
     mkdir $BACKUP_PATH/monthly_backups."$BACKUP_DATE"
     mkdir $BACKUP_PATH/monthly_backups."$BACKUP_DATE"/vm_backups."$BACKUP_DATE"
     mkdir $BACKUP_PATH/monthly_backups."$BACKUP_DATE"/lxc_backups."$BACKUP_DATE"
-    echo "$VM_FILES"
+    echo "$LXC_FILES"
 fi
