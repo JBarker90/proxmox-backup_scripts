c0ad92a054f077fcb89b2728344f1e7b89c0d6b5
JBarker09 <jonathan.barker09@gmail.com>
2022-12-13 12:23:38 -0500

    created new variable to find vm backups specifically

--- backup-archive_test.sh
+++ backup-archive_test.sh
@@ -4,6 +4,8 @@
 
 BACKUP_PATH="/home/jonathan/backup_test/dump"
 BACKUP_DATE="$(find "$BACKUP_PATH" -maxdepth 1 -type f -iname 'vzdump*' | awk -F '/' '{print $6}' | cut -d '-' -f 4 | sed 's/_/-/g' | sort | tail -n1)"
+VM_FILES="$(find "$BACKUP_PATH" -maxdepth 1 -type f -iname '*qemu*')"
+
 
 # This will check if there is a parent directory for the current backups and then create a directory
 if [[ -e "$BACKUP_PATH/monthly_backups.$BACKUP_DATE" ]]; then
@@ -14,4 +16,5 @@ else
     mkdir $BACKUP_PATH/monthly_backups."$BACKUP_DATE"
     mkdir $BACKUP_PATH/monthly_backups."$BACKUP_DATE"/vm_backups."$BACKUP_DATE"
     mkdir $BACKUP_PATH/monthly_backups."$BACKUP_DATE"/lxc_backups."$BACKUP_DATE"
+    echo "$VM_FILES"
 fi
