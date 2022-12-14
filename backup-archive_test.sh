a3b614feae0684e22504809a2be5e43ca76d48b3
JBarker09 <jonathan.barker09@gmail.com>
2022-12-14 12:19:07 -0500

    added exit code

--- backup-archive_test.sh
+++ backup-archive_test.sh
@@ -11,11 +11,12 @@ LXC_FILES="$(find "$BACKUP_PATH" -maxdepth 1 -type f -iname '*lxc*' | sort)"
 # This will check if there is a parent directory for the current backups and then create a directory
 if [[ -e "$BACKUP_PATH/monthly_backups.$BACKUP_DATE" ]]; then
     echo "This directory already exists..."
-
+    exit 1
 else
 
     mkdir $BACKUP_PATH/monthly_backups."$BACKUP_DATE"
     mkdir $BACKUP_PATH/monthly_backups."$BACKUP_DATE"/vm_backups."$BACKUP_DATE"
     mkdir $BACKUP_PATH/monthly_backups."$BACKUP_DATE"/lxc_backups."$BACKUP_DATE"
-    echo "$LXC_FILES"
+    
+    
 fi
